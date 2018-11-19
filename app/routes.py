497c48476ae59f5f39d7d0bf321b52ea34109ffe
Jeff Babio <babiojeff@gmail.com>
2018-11-19 14:09:23 -0500

    Home page completed merge with master

--- app/routes.py
+++ app/routes.py
@@ -7,5 +7,27 @@ from flask import render_template, url_for, redirect
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
-    
+    products = {
+    101:    {
+            "id": 101,
+            "title": "Soap",
+            "price": 4.95,
+            "url": "http://placehold.it/250x250",
+            'desc': "This bar of soap is a bar of soapy soap."
+            },
+    102:    {
+            "id": 102,
+            "title": "Grapes",
+            "price": 3.85,
+            "url": "http://placehold.it/250x250",
+            "desc": "These grapes are a bundle of grapey grapes."
+    },
+    103:    {
+            "id": 103,
+            "title": "Oranges",
+            "price": 67.85,
+            "url": "http://placehold.it/250x250",
+            "desc": "These oranges are a box of orangey oranges."
+            }
+    }
+    return render_template('index.html', products=products)
